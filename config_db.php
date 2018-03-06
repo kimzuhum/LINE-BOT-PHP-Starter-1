100bb7b03b6bca77fc045792cfda5e7d924733e6
kimzuhum <kimhun-y-za@hotmail.com>
2018-03-06 11:50:53 +0700

    Update config_db.php

--- config_db.php
+++ config_db.php
@@ -2,7 +2,11 @@
 date_default_timezone_set("Asia/Bangkok");
 
 $strAccessToken = "T0SP3C3GuTGXd1XKSWQ0HJrENyiAVRu4/fo8vHokDtmtU0qYCKTA1U7SwD9rykeIxTfkDDxuP4TZu7Eea0zr36O/dAQy18VvCoYVIImzkHVQQ8sAHdV+p17u/97Ud24almhqwXHSlKhAvvf83CF3FQdB04t89/1O/w1cDnyilFU=";
- 
+$servername = "office.winbb.net";
+$username = "root";
+$password = "hs5fe";
+$dbname = "pongpark";
+
 $content = file_get_contents('php://input');
 $arrJson = json_decode($content, true);
 
@@ -12,29 +16,33 @@ $arrHeader = array();
 $arrHeader[] = "Content-Type: application/json";
 $arrHeader[] = "Authorization: Bearer {$strAccessToken}";
 
-$objConnect = mysqli_connect("61.47.34.20","root","hs5fe","pongpark");
-
-if($objConnect){
-$sql = "SELECT * FROM tb_question WHERE ask = '" . $arrJson['events'][0]['message']['text'] . "'";
-$result = mysqli_query($objConnect,$sql);
-$row = mysqli_fetch_row($result);
-if(!isset($row['ans'])){
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    //die("Connection failed: " . $conn->connect_error);
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = $row['ans'];
+  $arrPostData['messages'][0]['text'] = "connect error";
+} 
+
+$sql = "SELECT * FROM tb_question WHERE txt_ask = '" . test . "'";
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    // output data of each row
+  $arrPostData = array();
+  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
+  $arrPostData['messages'][0]['type'] = "text";
+  $arrPostData['messages'][0]['text'] = $row["txt_ask"];
 }else{
   $arrPostData = array();
   $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
   $arrPostData['messages'][0]['type'] = "text";
   $arrPostData['messages'][0]['text'] = "ฉันไม่เข้าใจคำสั่ง";
 }
-}else{
-  $arrPostData = array();
-  $arrPostData['replyToken'] = $arrJson['events'][0]['replyToken'];
-  $arrPostData['messages'][0]['type'] = "text";
-  $arrPostData['messages'][0]['text'] = "Failed to connect.";
-}
+
+
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL,$strUrl);
 curl_setopt($ch, CURLOPT_HEADER, false);
@@ -46,5 +54,5 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
 $result = curl_exec($ch);
 curl_close ($ch);
 
-	mysqli_close($objConnect);
+$conn->close();
 ?>
