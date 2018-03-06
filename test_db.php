2d52382285d27cf06f55f6607ea0d9287b6cc100
kimzuhum <kimhun-y-za@hotmail.com>
2018-03-06 10:18:39 +0700

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,7 +1,7 @@
 <?php
 $objConnect = mysqli_connect("61.47.34.20","root","hs5fe","pongpark");
 if($objConnect){
-$sql = "SELECT * FROM tb_question WHERE ask = '" . Test . "'";
+echo $sql = "SELECT * FROM tb_question WHERE ask = '" . Test . "'";
 $result = mysqli_query($objConnect,$sql);
 $row = mysqli_fetch_row($result);
 if(!isset($row['ans'])){
