9e0593b4d910ea441d4dc017fcac3ec973391b01
kimzuhum <kimhun-y-za@hotmail.com>
2018-03-06 11:18:48 +0700

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,13 +1,13 @@
 <?php
 date_default_timezone_set("Asia/Bangkok");
 
-$conn = mysql_connect('61.47.34.20', 'root', 'hs5fe') or die('Could not connect: ' . mysql_error());
-$db_selected = mysql_select_db('pongpark', $conn) or die ('Can\'t use Database : ' . mysql_error());
-mysql_query("SET NAMES 'tis620'");
+$conn = mysqli_connect('61.47.34.20', 'root', 'hs5fe') or die('Could not connect: ' . mysql_error());
+$db_selected = mysqli_select_db('pongpark', $conn) or die ('Can\'t use Database : ' . mysql_error());
+mysqli_query("SET NAMES 'tis620'");
 
 	$strSQL = "SELECT * FROM tb_question WHERE txt_ask = 'test'";
-	$objQuery = mysql_query($strSQL);
-	$obj_row = mysql_fetch_array($objQuery);
+	$objQuery = mysqli_query($strSQL);
+	$obj_row = mysqli_fetch_array($objQuery);
 
 echo "<br/>ASN : " . $obj_row['txt_ans'];
 ?>
