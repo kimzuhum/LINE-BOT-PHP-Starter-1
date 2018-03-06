a6e5238fcb965f73d94579609d46633188cd3a4f
kimzuhum <kimhun-y-za@hotmail.com>
2018-03-06 10:16:57 +0700

    Create test_db.php

---
+++ test_db.php
@@ -0,0 +1,17 @@
+<?php
+$objConnect = mysqli_connect("61.47.34.20","root","hs5fe","pongpark");
+if($objConnect){
+$sql = "SELECT * FROM tb_question WHERE ask = '" . $_GET['text'] . "'";
+$result = mysqli_query($objConnect,$sql);
+$row = mysqli_fetch_row($result);
+if(!isset($row['ans'])){
+  echo $row['ans'];
+}else{
+  echo "ฉันไม่เข้าใจคำสั่ง";
+}
+}else{
+  echo "Failed to connect.";
+}
+	mysqli_close($objConnect);
+  
+  ?>
