ac6b4b4ad26a1d6edcab2f3f28c694ff0a1f48ec
kimzuhum <kimhun-y-za@hotmail.com>
2018-03-06 11:25:17 +0700

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -2,7 +2,7 @@
 date_default_timezone_set("Asia/Bangkok");
 
 $conn = mysqli_connect('61.47.34.20', 'root', 'hs5fe') or die('Could not connect: ' . mysql_error());
-$db_selected = mysql_select_db('pongpark', $conn) or die ('Can\'t use Database : ' . mysql_error());
+$db_selected = mysqli_select_db("pongpark", $conn) or die ('Can\'t use Database : ' . mysql_error());
 mysql_query("SET NAMES 'tis620'");
 
 	$strSQL = "SELECT * FROM tb_question WHERE txt_ask = 'test'";
