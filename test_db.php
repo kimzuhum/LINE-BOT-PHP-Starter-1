a74812b443b2b49d6ffa3b31a544013106cc107f
kimzuhum <kimhun-y-za@hotmail.com>
2018-03-06 11:30:11 +0700

    Update test_db.php

--- test_db.php
+++ test_db.php
@@ -1,6 +1,6 @@
 <?php
 date_default_timezone_set("Asia/Bangkok");
-
+/*
 $conn = mysqli_connect('61.47.34.20', 'root', 'hs5fe') or die('Could not connect: ' . mysql_error());
 $db_selected = mysqli_select_db("pongpark", $conn) or die ('Can\'t use Database : ' . mysql_error());
 mysql_query("SET NAMES 'tis620'");
@@ -10,4 +10,30 @@ mysql_query("SET NAMES 'tis620'");
 	$obj_row = mysql_fetch_array($objQuery);
 
 echo "<br/>ASN : " . $obj_row['txt_ans'];
+*/
+
+$servername = "61.47.34.20";
+$username = "root";
+$password = "hs5fe";
+$dbname = "pongpank";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+$sql = "SELECT * FROM tb_question";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    // output data of each row
+    while($row = $result->fetch_assoc()) {
+        echo "id: " . $row["ques_id"]. " - ask: " . $row["txt_ask"]. " ans " . $row["txt_ans"]. "<br>";
+    }
+} else {
+    echo "0 results";
+}
+$conn->close();
 ?>
